837b9f34b395f90a4355bbd4057b200120cd423b
IrinaSkrDev <plat.iren@gmail.com>
2020-12-10 14:49:23 +0300

    update field test

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,7 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+import io.hexlet.xo.model.exceptions.PointAlreadyOccupaidEcxeption;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -23,7 +25,7 @@ class FieldTest {
     // }
 
     @Test
-    void setFigure() {
+    void setFigure() throws InvalidPointException, PointAlreadyOccupaidEcxeption {
         final Field field = new Field();
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.X;
